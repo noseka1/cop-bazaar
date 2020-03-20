265aab0b4abedd21825416c21231d176225f14f5
Ales Nosek <ales.nosek@gmail.com>
2020-03-20 12:22:29 -0700

    Handle GitHubs rate limiting

--- model/repository.py
+++ model/repository.py
@@ -23,13 +23,14 @@ class Repository(object):
             repo_path = self.url[len(GITHUB_REPO_PREFIX):]
             repo_info = requests.get('%s%s' % (GITHUB_API, repo_path))
             repo_json = repo_info.json()
-
-            #pp = pprint.PrettyPrinter(indent=4)
-            #pp.pprint(repo_json)
-
-            self.data['repo_path'] = repo_path
-            self.data['description'] = repo_json['description']
-            self.data['html_url'] = repo_json['html_url']
-            self.data['pushed_at'] = repo_json['pushed_at']
-            self.data['stargazers_count'] = repo_json['stargazers_count']
-            self.data['forks_count'] = repo_json['forks_count']
+            if repo_info.status_code == 200:
+                self.data['repo_path'] = repo_path
+                self.data['description'] = repo_json['description']
+                self.data['html_url'] = repo_json['html_url']
+                self.data['pushed_at'] = repo_json['pushed_at']
+                self.data['stargazers_count'] = repo_json['stargazers_count']
+                self.data['forks_count'] = repo_json['forks_count']
+            else:
+                pp = pprint.PrettyPrinter(indent=4)
+                raise Exception("Failed to fetch data for repo %s. Response was: %s" %
+                    (self.url, pp.pformat(repo_json)))
